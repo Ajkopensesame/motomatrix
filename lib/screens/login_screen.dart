1e73ab82f68db41816877e6d4e2162e76e7afa94
joshkomant <joshkomant@gmail.com>
2024-01-08 20:32:02 +1000

    Update dependencies and fix code issues

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,28 +1,34 @@
+// ignore_for_file: use_build_context_synchronously
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:motomatrix/screens/signup_screen.dart';
 import '../main.dart';
-import '../widgets/custom_app_bar.dart'; // <-- Import the CustomAppBar
+import '../widgets/custom_app_bar.dart';
 import '../widgets/custom_elevatedbutton.dart';
 import '../widgets/custom_textbutton.dart';
-import '../widgets/custom_textfield.dart'; // <-- Import the CustomTextField
-import '../widgets/custom_background.dart'; // <-- Import the CustomBackground
+import '../widgets/custom_textfield.dart';
+import '../widgets/custom_background.dart';
 
-class LoginScreen extends ConsumerWidget {
-  final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
-
-  LoginScreen({super.key});
+class LoginScreen extends ConsumerStatefulWidget {
+  const LoginScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  // ignore: library_private_types_in_public_api
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends ConsumerState<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool _isPasswordError = false; // New variable to track password error state
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const CustomAppBar(
-        // <-- Use the CustomAppBar here
-        title: 'Login',
-      ),
+      appBar: const CustomAppBar(title: 'Login'),
       body: CustomBackground(
-        // <-- Use the CustomBackground here
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Form(
@@ -30,62 +36,25 @@ class LoginScreen extends ConsumerWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 CustomTextField(
-                  // <-- Use the CustomTextField here
                   controller: _emailController,
                   labelText: 'Email',
                   keyboardType: TextInputType.emailAddress,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your email';
-                    }
-                    if (!value.contains('@')) {
-                      return 'Please enter a valid email';
-                    }
-                    return null;
-                  },
                 ),
                 const SizedBox(height: 16),
                 CustomTextField(
-                  // <-- Use the CustomTextField here
                   controller: _passwordController,
                   labelText: 'Password',
                   obscureText: true,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your password';
-                    }
-                    if (value.length < 6) {
-                      return 'Password must be at least 6 characters long';
-                    }
-                    return null;
-                  },
+                  borderColor: _isPasswordError
+                      ? Colors.red
+                      : null, // Border color change
                 ),
                 const SizedBox(height: 32),
                 CustomElevatedButton(
-                  // <-- Use the CustomElevatedButton here
                   label: 'Login',
-                  onPressed: () async {
-                    try {
-                      final user = await ref
-                          .read(firebaseAuthProvider)
-                          .signInWithEmailAndPassword(
-                            _emailController.text,
-                            _passwordController.text,
-                          );
-                      if (user != null) {
-                        // ignore: use_build_context_synchronously
-                        Navigator.pushReplacementNamed(context, '/dashboard');
-                      }
-                    } catch (error) {
-                      // ignore: use_build_context_synchronously
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(content: Text('Login failed: $error')),
-                      );
-                    }
-                  },
+                  onPressed: () => _loginPressed(context),
                 ),
                 CustomTextButton(
-                  // <-- Use the CustomTextButton here
                   label: "Don't have an account? Sign up",
                   onPressed: () {
                     Navigator.of(context).push(
@@ -100,5 +69,39 @@ class LoginScreen extends ConsumerWidget {
         ),
       ),
     );
+  }
+
+  void _loginPressed(BuildContext context) async {
+    final email = _emailController.text.trim();
+    final password = _passwordController.text.trim();
+
+    if (email.isEmpty && password.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Please enter email and password')),
+      );
+      return;
+    }
+
+    if (email.isEmpty || password.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Please fill out all fields')),
+      );
+      return;
+    }
+
+    try {
+      final user = await ref
+          .read(firebaseAuthProvider)
+          .signInWithEmailAndPassword(email, password);
+      if (user != null) {
+        Navigator.pushReplacementNamed(context, '/dashboard');
+      }
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'wrong-password') {
+        setState(() {
+          _isPasswordError = true; // Set password error state to true
+        });
+      }
+    }
   }
 }
